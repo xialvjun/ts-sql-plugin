9d04652d30782f313743ddd9e819442e4b222772
Vladislav Botvin <darkvlados@gmail.com>
2020-05-20 17:52:05 +0300

    watch mode support

--- src/cli.ts
+++ src/cli.ts
@@ -7,15 +7,10 @@ import * as child_process from "child_process";
 
 import ts from "typescript";
 import commander from "commander";
+import chokidar from "chokidar";
+import debounce from "lodash.debounce";
 
-import {
-  default_command,
-  default_tags,
-  default_cost_pattern,
-  get_all_ts_files,
-  report,
-  index_of_array,
-} from "./utils";
+import { default_command, default_tags, default_cost_pattern, get_all_ts_files, report, index_of_array } from "./utils";
 import { make_fake_expression } from "./make_fake_expression";
 import { parseDirectives } from "./directiveParser";
 
@@ -25,30 +20,15 @@ const default_tags_string = Object.entries(default_tags)
   .join(",");
 
 commander
-  .option(
-    "-p, --project <string>",
-    "The project path or tsconfig.json, defaults to: ./ .",
-    "./"
-  )
-  .option(
-    "-e, --exclude <regexp>",
-    "The regexp to exclude files, defaults to: node_modules .",
-    "node_modules"
-  )
+  .option("-p, --project <string>", "The project path or tsconfig.json, defaults to: ./ .", "./")
+  .option("-e, --exclude <regexp>", "The regexp to exclude files, defaults to: node_modules .", "node_modules")
   .option(
     "-t, --tags <string>",
     `The tags you used in you ts file, defaults to: ${default_tags_string} .`,
     default_tags_string
   )
-  .option(
-    "-m, --error-cost <int>",
-    "Throw error if explain cost exceeds treshold."
-  )
-  .option(
-    "--emit-out-dir <string>",
-    "path, where sqls will be emitted",
-    "./emit-sql"
-  )
+  .option("-m, --error-cost <int>", "Throw error if explain cost exceeds treshold.")
+  .option("--emit-out-dir <string>", "Path, where sqls will be emitted", "./emit-sql")
   .option("--warn-cost <int>", "Log warning if explain cost exceeds treshold.")
   .option("--info-cost <int>", "Log info if explain cost exceeds treshold.")
   .option(
@@ -56,22 +36,17 @@ commander
     `The regexp used to extract cost from command stdout, defaults to: ${default_cost_pattern_source} .`,
     default_cost_pattern_source
   )
+  .option("-w, --watch", `Watch mode of cli`, false)
   .arguments("[command...]")
-  .description(
-    "Explain all your sqls in your code to test them. Eg: ts-sql-plugin -p ./my_ts_projet psql -c",
-    {
-      command: "The command to be run to explain the faked sql, like: psql.",
-      args:
-        "The arguments passed to the command, like: -c. The faked sql will be added as the last argument.",
-    }
-  )
+  .description("Explain all your sqls in your code to test them. Eg: ts-sql-plugin -p ./my_ts_projet psql -c", {
+    command: "The command to be run to explain the faked sql, like: psql.",
+    args: "The arguments passed to the command, like: -c. The faked sql will be added as the last argument.",
+  })
   .action((_command) => {
     if (_command.length === 0) {
       _command = default_command;
     } else {
-      _command = commander.rawArgs.slice(
-        index_of_array(commander.rawArgs, _command)
-      );
+      _command = commander.rawArgs.slice(index_of_array(commander.rawArgs, _command));
     }
 
     const config = commander.opts();
@@ -102,16 +77,43 @@ commander
       fs.readFileSync(tsconfig_path, { encoding: "utf8" })
     );
 
-    const program = ts.createProgram(get_all_ts_files(project_path), tsconfig);
+    const watchHost = config.watch
+      ? ts.createWatchCompilerHost(get_all_ts_files(project_path), tsconfig.compilerOptions, ts.sys)
+      : null;
 
-    const fake_expression = make_fake_expression(
-      program.getTypeChecker(),
-      tags
-    );
+    const watchProgram = config.watch ? ts.createWatchProgram(watchHost) : null;
+
+    const initProgram = config.watch
+      ? watchProgram.getProgram().getProgram()
+      : ts.createProgram(get_all_ts_files(project_path), tsconfig.compilerOptions);
+
+    let fake_expression = make_fake_expression(initProgram.getTypeChecker(), tags);
+
+    if (config.watch) {
+      let changedFile = "";
+      chokidar.watch(`${project_path}/**/*.{ts,tsx}`).on("all", ({}, path) => {
+        changedFile = path;
+      });
+      const origOnWatch = watchHost.onWatchStatusChange;
+      const onChangeFile = debounce(
+        () => {
+          const file = watchProgram.getProgram().getSourceFile(path.join(project_path, changedFile));
+          if (file && !exclude.test(file.fileName)) {
+            fake_expression = make_fake_expression(watchProgram.getProgram().getProgram().getTypeChecker(), tags);
+            delint(file);
+          }
+        },
+        2000
+      );
+      watchHost.onWatchStatusChange = (...args) => {
+        origOnWatch.apply(watchHost, args);
+        onChangeFile();
+      };
+    }
 
     let has_error = false;
 
-    program.getSourceFiles().forEach((f) => {
+    initProgram.getSourceFiles().forEach((f) => {
       if (!exclude.test(f.fileName)) {
         delint(f);
       }
@@ -135,22 +137,15 @@ commander
               const directives = parseDirectives(s);
               const emitDir = directives.find((d) => d.directive === "emit");
               if (emitDir) {
-                const fileName =
-                  (emitDir.arg as string) ??
-                  crypto.createHash("sha1").update(s).digest("hex");
+                const fileName = (emitDir.arg as string) ?? crypto.createHash("sha1").update(s).digest("hex");
                 const filePath = `${config.emitOutDir}/${fileName}.sql`;
                 fs.writeFile(filePath, s, (err) => {
                   if (err) {
-                    console.error(
-                      `Error occured, when emitting file "${filePath}"`
-                    );
+                    console.error(`Error occured, when emitting file "${filePath}"`);
                   }
                 });
               }
-              let p = child_process.spawnSync(
-                _command[0],
-                _command.slice(1).concat(`EXPLAIN ${s}`)
-              );
+              let p = child_process.spawnSync(_command[0], _command.slice(1).concat(`EXPLAIN ${s}`));
               if (p.status) {
                 const stderr_str = (p.stderr.toString as any)("utf8");
                 has_error = true;
@@ -159,7 +154,7 @@ commander
               }
               if (
                 (config.error_cost || config.warn_cost || config.info_cost) &&
-                !directives.some((d) => (d.directive === "ignore-cost"))
+                !directives.some((d) => d.directive === "ignore-cost")
               ) {
                 const stdout_str = (p.stdout.toString as any)("utf8");
                 const match = stdout_str.match(cost_pattern);
@@ -168,42 +163,16 @@ commander
                   const cost = Number(cost_str);
                   if (cost > config.error_cost) {
                     has_error = true;
-                    report(
-                      sourceFile,
-                      node,
-                      `Error: explain cost is too high: ${cost}\n${s}`,
-                      3
-                    );
+                    report(sourceFile, node, `Error: explain cost is too high: ${cost}\n${s}`, 3);
                     break;
                   } else if (cost > config.warn_cost) {
-                    report(
-                      sourceFile,
-                      node,
-                      `Warn: explain cost is at warning: ${cost}\n${s}`,
-                      2
-                    );
+                    report(sourceFile, node, `Warn: explain cost is at warning: ${cost}\n${s}`, 2);
                   } else if (cost > config.info_cost) {
-                    report(
-                      sourceFile,
-                      node,
-                      `Info: explain cost is ok: ${cost}\n${s}`,
-                      1
-                    );
+                    report(sourceFile, node, `Info: explain cost is ok: ${cost}\n${s}`, 1);
                   }
                 }
               }
             }
-            // query_configs.map((qc: any) => {
-            //   let s = qc.text.replace(/\?\?/gm, 'null');
-            //   let p = child_process.spawnSync(
-            //     _command,
-            //     _args.concat(`EXPLAIN ${s}`),
-            //   );
-            //   if (p.status) {
-            //     has_error = true;
-            //     report(sourceFile, node, stdout_str);
-            //   }
-            // });
           }
         }
 
